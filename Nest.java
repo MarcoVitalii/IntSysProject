5df3340d8a6adab23d3d34a53f02509c083db779
Marco Vitali <ilmarcvit@gmail.com>
2018-10-03 15:13:38 +0200

    added foodRecovered to chart performance

--- Nest.java
+++ Nest.java
@@ -5,4 +5,6 @@ import sim.util.*;
 public class Nest
 {
     //it will have something inside at some point
+    public int foodRecovered;
+    public int getFoodRecovered() {return foodRecovered;}
 }
